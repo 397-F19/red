f43bb8f6a7225162ff357c5da677b4d9103ac291
Danyil Pysmak <d.pysmak@gmail.com>
2019-10-16 00:59:07 -0500

    changing id to be 7 symbols long

--- backend/server.js
+++ backend/server.js
@@ -138,7 +138,7 @@ function makeid() {
 	var possible =
 		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-	for (var i = 0; i < 5; i++)
+	for (var i = 0; i < 7; i++)
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 
 	return text;
