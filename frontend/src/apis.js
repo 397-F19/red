1048dab0420d21dedaebfbc48996f443bc3d58b6
Bradley Ramos <bradleymramos@gmail.com>
2019-11-02 21:45:03 -0400

    Add useful APIs to frontend

--- frontend/src/apis.js
+++ frontend/src/apis.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const root = 'localhost:5000';
+
 export async function getRequest(route) {
 	const res = await axios.get(route);
 	if (res.status !== 200) {
@@ -21,20 +23,51 @@ export async function postRequest(route, data = null) {
 	}
 }
 
-export async function createUser(data) {
-	// Example postRequest with data. Replace static with form input
-	postRequest('/users', {
-		uid: data.uid,
-		name: data.name,
-		email: data.email,
-		avatar: data.avatar
-	})
-		.then(res => {
-			const code = res.code;
-			console.log(code);
-			return true;
-		})
-		.catch(err => console.log(err));
+export async function getUserEvents(user) {
+	const res = await axios.get(root+'/events/attendee', { 'headers': { 'uid': user } });
+	if (res.status !== 200) {
+		throw Error(res.message);
+	}
+	return res.data;
+}
+
+export async function createEvent(data) {
+	if (!data) {
+		throw Error('Cannot send post request without data');
+	} else {
+		const res = await axios.post(root+'/events', data);
+
+		if (res.status !== 200) {
+			throw Error(res.message);
+		}
+		return res.data;
+	}
+}
+
+export async function createEvent(data) {
+	if (!data) {
+		throw Error('Cannot send post request without data');
+	} else {
+		const res = await axios.post(root+'/events', data);
+
+		if (res.status !== 200) {
+			throw Error(res.message);
+		}
+		return res.data;
+	}
+}
+
+export async function deleteEvent(event_id) {
+	if (!data) {
+		throw Error('Cannot send post request without data');
+	} else {
+		const res = axios.delete(root+'/events/id', { 'headers': { 'id': event_id } });
+
+		if (res.status !== 200) {
+			throw Error(res.message);
+		}
+		return res.data;
+	}
 }
 
 export async function getUserInfo(uid) {
